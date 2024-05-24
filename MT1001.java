376a74cbc3fe7b2ede5a6e5ad11021fc4ec0df84
Gin5567 <12210531@mail.sustech.edu.cn>
2024-05-24 09:06:18 +0800

    testing codes

---
+++ MT1001.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+import java.util.*;
+
+
+public class MT1001 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("This is my first program!");
+        System.out.println("Coding is fun!");
+        System.out.println("Files can be changed and saved!");
+        System.out.println("Git branch create and merge!");
+    }
+}
